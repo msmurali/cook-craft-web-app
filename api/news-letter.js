02f39d18bb36b2b89f1930233fd361a016bc52f1
msmurali <muralikrishnan0511@gmail.com>
2024-09-16 21:27:14 +0200

    Fix: Mail gun Changes

--- api/news-letter.js
+++ api/news-letter.js
@@ -1,44 +1,46 @@
-const { Client } = require('pg');
-const mailgun = require('mailgun-js');
-
+const { Pool } = require("pg");
+const mailgun = require("mailgun-js");
 
 const mg = mailgun({
   apiKey: process.env.MAILGUN_API_KEY,
   domain: process.env.MAILGUN_DOMAIN,
 });
 
-
-const client = new Client({
+const pool = new Pool({
   connectionString: process.env.POSTGRES_CONNECTION_STRING,
 });
 
 module.exports = async function handler(req, res) {
-  if (req.method === 'POST') {
+  if (req.method === "POST") {
+    let client;
     try {
-      await client.connect();
-      const result = await client.query('SELECT email FROM subscribers');
-      await client.end();
+      client = await pool.connect();
+      const result = await client.query("SELECT email FROM subscribers");
 
       const emails = result.rows.map((row) => row.email);
 
       const emailData = {
-        from: 'Excited User <sandbox703cc3df5a4f4a96a76d65b67b9edc4a.mailgun.org>', 
-        to: emails.join(','), 
-        subject: 'Weekly Newsletter',
-        text: 'Here is your weekly newsletter',
+        from: "Excited User <sandbox703cc3df5a4f4a96a76d65b67b9edc4a.mailgun.org>",
+        to: emails.join(","),
+        subject: "Weekly Newsletter",
+        text: "Here is your weekly newsletter",
       };
 
       const mailResponse = await mg.messages().send(emailData);
-      
-      return res.status(200).json({ message: 'Newsletter sent successfully', mailResponse });
 
+      return res
+        .status(200)
+        .json({ message: "Newsletter sent successfully", mailResponse });
     } catch (error) {
-      console.error('Error occurred:', error);
-      return res.status(500).json({ error: 'Something went wrong, try again later' });
+      console.error("Error occurred:", error);
+      return res
+        .status(500)
+        .json({ error: "Something went wrong, try again later" });
+    } finally {
+      if (client) client.release(); // Release the client back to the pool
     }
   } else {
-
-    res.setHeader('Allow', ['POST']);
+    res.setHeader("Allow", ["POST"]);
     return res.status(405).json({ error: `Method ${req.method} Not Allowed` });
   }
 };
