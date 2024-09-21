3f467b74edec308e8a64fb169683151e38849244
msmurali <muralikrishnan0511@gmail.com>
2024-09-21 12:52:24 +0200

    Fix: Spinner changes

--- src/shared/components/spinner/spinner.component.ts
+++ src/shared/components/spinner/spinner.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { tap } from 'rxjs';
 import { SpinnerService } from 'src/shared/services/spinner.service';
 
 @Component({
   selector: 'app-spinner',
   template: `
     <div *ngIf="isLoading$ | async" class="spinner-overlay">
-      <div class="spinner"></div>
+      <div class="spinner">{{ isLoading$ | async }}</div>
     </div>
   `,
   styleUrls: ['./spinner.component.scss'],
@@ -13,5 +14,7 @@ import { SpinnerService } from 'src/shared/services/spinner.service';
 export class SpinnerComponent {
   isLoading$ = this.spinnerService.loading$;
 
-  constructor(private spinnerService: SpinnerService) {}
+  constructor(private spinnerService: SpinnerService) {
+    this.isLoading$.pipe(tap(console.log)).subscribe();
+  }
 }
